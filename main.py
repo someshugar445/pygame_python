9e81b013fd1b1f4a5e7dd9f674b5434beba1517d
someshugar445 <Kamala@096>
2020-08-16 20:47:07 +0530

    added start finish buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
-pygame.init()
 import random
+pygame.init()
 display_width = 800
 display_height = 600
 
@@ -11,13 +11,20 @@ pygame.display.set_caption('Snake Game')
 black = (0, 0, 0)
 brown = (230, 160, 98)
 snake_width = 73
+red = (200, 0, 0)
+green = (0, 200, 0)
+
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+
 clock = pygame.time.Clock()
 crashed = False
 snake_img = pygame.image.load('snake.png')
 
 
 def things(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+    pygame.draw.rect(gameDisplay, red, [thingx, thingy, thingw, thingh])
+
 
 def snake(x, y):
     gameDisplay.blit(snake_img, (x, y))
@@ -28,21 +35,117 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf', 115)
-    TextSurf, TextRect = text_objects(text, largeText)
+def crash():
+    largeText = pygame.font.SysFont("comicsansms", 115)
+    TextSurf, TextRect = text_objects("You Died", largeText)
     TextRect.center = ((display_width / 2), (display_height / 2))
     gameDisplay.blit(TextSurf, TextRect)
 
-    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    time.sleep(2)
+        # gameDisplay.fill(white)
 
-    game_loop()
+        button("Restart", 150, 450, 100, 50, green, bright_green, game_loop)
+        button("Exit", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
 
 
-def crash():
-    message_display('You Crashed')
+def button(msg, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    # print(click)
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+    smallText = pygame.font.SysFont("comicsansms", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    gameDisplay.blit(textSurf, textRect)
+
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+def unpause():
+    global pause
+    pause = False
+
+
+def paused():
+    largeText = pygame.font.SysFont("comicsansms", 115)
+    TextSurf, TextRect = text_objects("Paused", largeText)
+    TextRect.center = ((display_width / 2), (display_height / 2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    while pause:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # gameDisplay.fill(white)
+
+        button("Unpause", 150, 450, 100, 50, green, bright_green, unpause)
+        button("Exit", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(brown)
+        largeText = pygame.font.SysFont("comicsansms", 115)
+        TextSurf, TextRect = text_objects("Snake Game", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("Start", 150, 450, 100, 50, green, bright_green, game_loop)
+        button("Finish!!!", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+
+        mouse = pygame.mouse.get_pos()
+
+        # print(mouse)
+
+        if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
+            pygame.draw.rect(gameDisplay, bright_green, (150, 450, 100, 50))
+        else:
+            pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("Start", smallText)
+        textRect.center = ((150 + (100 / 2)), (450 + (50 / 2)))
+        gameDisplay.blit(textSurf, textRect)
+
+        pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
+
+        pygame.display.update()
+        clock.tick(15)
+
 
 def game_loop():
     x = (display_width * 0.45)
@@ -100,6 +203,7 @@ def game_loop():
         clock.tick(60)
 
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
